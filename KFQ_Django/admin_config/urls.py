515f5d850bc2a996cc42a1ca518f94e768a20c39
김민서 <blackcode-@naver.com>
2021-07-06 17:37:31 +0900

    127.0.0.1 기본경로 원복

--- KFQ_Django/admin_config/urls.py
+++ KFQ_Django/admin_config/urls.py
@@ -21,8 +21,9 @@ from CRM import views as viewCRM
 
 urlpatterns = [
 
-    path('',viewCRM.Crm.index),
+    path('',Theme.theme_index),
     path('admin/', admin.site.urls),
+
 
     #Brand 관련 페이지
     path('BRAND/', include('BRAND.urls')),
